7522ef705808e1eab642918ccf60a06472da7d81
wudeyong <deyong1994@163.com>
2019-08-04 21:51:57 +0800

    Update handler.lua

--- kong/plugins/aliyun-http-filter/handler.lua
+++ kong/plugins/aliyun-http-filter/handler.lua
@@ -1,26 +1,17 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local responses = require "kong.tools.responses"
+local response = require "kong.response"
 
 local HttpFilterHandler = BasePlugin:extend()
 
 function HttpFilterHandler:new()
-  HttpFilterHandler.super.new(self, "aliyun-http-filter")
+  HttpFilterHandler.super.new(self, "https-filter")
 end
 
 function HttpFilterHandler:access(conf)
   HttpFilterHandler.super.access(self)
 
-  local filter = {
-    http = conf.http,
-    https = conf.https
-  }
-  local headers = ngx.req.get_headers()
-  local proto = headers["x-forwarded-proto"]
-  if not filter[proto] then
-    ngx.status = 403
-    ngx.header["Content-Type"] = "application/json; charset=utf-8"
-    ngx.say(string.format("{\"message\":\"%s protocol not supported\"}", proto))
-    return ngx.exit(ngx.status)
+  if ngx.var.https ~= "on" then
+    return ngx.redirect("https://" .. ngx.var.host .. ngx.var.request_uri, ngx.HTTP_MOVED_PERMANENTLY) 
   end
 end
 
